19d570691882b7f87de1cfbdb02cdb1a3b7cb86e
neve7mind <134313494+neve7mind@users.noreply.github.com>
2023-10-02 14:16:06 +0300

    Update Lab_3. Task_46.cpp

--- Lab_3. Task_46.cpp
+++ Lab_3. Task_46.cpp
@@ -8,19 +8,22 @@ int main()
 {
     setlocale (LC_ALL, "RU");
     float h;
-    cin >> h;
-    if (h >= 0 && h <= 12)
-        cout << "Тропосфера";
-    else if (h >= 12 && h <= 50)
-        cout << "Стратосфера";
-    else if (h >= 50 && h <= 90)
-        cout << "Мезосфера";
-    else if (h >= 90 && h <= 500)
-        cout << "Термосфера";
-    else if (h >= 500 && h <= 2500)
-        cout << "Экзосфера";
-    else 
-        cout << "Нет опр. слоя атмосферы";
-        
+    do {
+        cin >> h;
+        if (h >= 0 && h <= 12)
+            cout << "Тропосфера";
+        else if (h >= 12 && h <= 50)
+            cout << "Стратосфера";
+        else if (h >= 50 && h <= 90)
+            cout << "Мезосфера";
+        else if (h >= 90 && h <= 500)
+            cout << "Термосфера";
+        else if (h >= 500 && h <= 2500)
+            cout << "Экзосфера";
+        else 
+            cout << "Нет опр. слоя атмосферы\n";
+    }
+    while (h < 0 || h > 2500);
+    
     return 0;
 }
