880d48b0731cb8446d2b4510c7adc10bca6561d4
neve7mind <134313494+neve7mind@users.noreply.github.com>
2023-10-02 14:13:05 +0300

    Update Lab_3. Task_16.cpp

--- Lab_3. Task_16.cpp
+++ Lab_3. Task_16.cpp
@@ -10,50 +10,53 @@ int main()
 {
     setlocale (LC_ALL, "RU");
     unsigned int n;
-    cin >> n;
-    if (n >= 1 && n <= 1188) {
-        unsigned int y = n/12;
-        cout << y;
-        if (y%100 >= 11 && y%100 <= 19)
-            cout << " лет ";
-        else {
-            switch (y%10) {
-                case 0:
-                    cout << " лет ";
-                    break;
-                case 1:
-                    cout << " год ";
-                    break;
-                case 2 ... 4:
-                    cout << " года ";
-                    break;
-                case 5 ... 9:
-                    cout << " лет ";
-                    break;
+    do {
+        cin >> n;
+        if (n >= 1 && n <= 1188) {
+            unsigned int y = n/12;
+            cout << y;
+            if (y%100 >= 11 && y%100 <= 19)
+                cout << " лет ";
+            else {
+                switch (y%10) {
+                    case 0:
+                        cout << " лет ";
+                        break;
+                    case 1:
+                        cout << " год ";
+                        break;
+                    case 2 ... 4:
+                        cout << " года ";
+                        break;
+                    case 5 ... 9:
+                        cout << " лет ";
+                        break;
+                }
+            }
+            unsigned int m = n%12;
+            if (m%100 == 11)
+                cout << m << " месяцев";
+            else {
+                switch (m%10) {
+                    case 0:
+                        cout << "ровно";
+                        break;
+                    case 1:
+                        cout << m << " месяц";
+                        break;
+                    case 2 ... 4:
+                        cout << m << " месяца";
+                        break;
+                    case 5 ... 9:
+                        cout << m << " месяцев";
+                        break;
+                }
             }
         }
-        unsigned int m = n%12;
-        if (m%100 == 11)
-            cout << m << " месяцев";
         else {
-            switch (m%10) {
-                case 0:
-                    cout << "ровно";
-                    break;
-                case 1:
-                    cout << m << " месяц";
-                    break;
-                case 2 ... 4:
-                    cout << m << " месяца";
-                    break;
-                case 5 ... 9:
-                    cout << m << " месяцев";
-                    break;
-            }
+            cout << "Люди столько не живут\n";
         }
     }
-    else {
-        cout << "Люди столько не живут";
-    }
+    while (n >= 1188);
     return 0;
 }
