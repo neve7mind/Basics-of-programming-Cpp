d94791a34f59414e26b0504b475055e27e7c2a69
neve7mind <134313494+neve7mind@users.noreply.github.com>
2023-10-29 18:54:04 +0300

    Create Lab_6. Task_1.cpp

---
+++ Lab_6. Task_1.cpp
@@ -0,0 +1,23 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*Дано целое число N. Сформировать и вывести целочисленный массив размера N, содержащий N первых положительных 
+нечетных чисел: 1, 3, 5, … .*/
+
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    
+    ll n, k=1;
+    cin >> n;
+    ll narr[n];
+    for(int i=0; i<n; i++)
+    {
+        narr[i] = k;
+        k+=2;
+        cout << narr[i] << " ";
+    }
+    
+    return 0;
+}
