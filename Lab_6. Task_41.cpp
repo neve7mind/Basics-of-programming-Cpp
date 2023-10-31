0bf52cec3d546d7986cf1f420fc8d558328551f9
neve7mind <134313494+neve7mind@users.noreply.github.com>
2023-10-31 03:03:11 +0300

    Create Lab_6. Task_41.cpp

---
+++ Lab_6. Task_41.cpp
@@ -0,0 +1,35 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*41. Дан массив размера N (N>1). Найти два соседних элемента, сумма которых максимальна, и вывести эти элементы 
+в порядке возрастания их индексов.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int n;
+    double temp1 = 0, temp2 = 0;
+    cin >> n;
+    if (n>1)
+    {
+        double arr[n];
+        for (unsigned int i=0; i<n; i++)
+            cin >> arr[i];
+        double sum = arr[0]+arr[1];
+        temp1 = arr[0];
+        temp2 = arr[1];
+        for (unsigned int i=1; i<n-1; i++)
+        {
+            double sum1 = arr[i]+arr[i+1];
+            if (sum < sum1) 
+            {
+                sum = sum1;
+                temp1 = arr[i];
+                temp2 = arr[i+1];
+            }
+        }
+        cout << temp1 << " " << temp2;
+    }
+    
+    return 0;
+}
