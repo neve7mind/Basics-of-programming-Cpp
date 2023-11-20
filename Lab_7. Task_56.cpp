51ab239f9f9f6eed1466c2978ea4a805384809bf
neve7mind <134313494+neve7mind@users.noreply.github.com>
2023-11-20 11:48:06 +0300

    Create Lab_7. Task_56.cpp

---
+++ Lab_7. Task_56.cpp
@@ -0,0 +1,27 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*56. Дана матрица размера M×N (N — четное число). Поменять местами левую и правую половины матрицы.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int M, N;
+    cin >> M >> N;
+    float arr[M][N];
+    for(int i=0; i<M; i++)
+    {
+        for(int j=0; j<N; j++)
+            cin >> arr[i][j];
+    }
+    for (int i=0; i<M; i++)
+    {
+        for (int j=(N/2); j<N; j++)
+            cout << arr[i][j] << " ";
+        for(int k=0; k<(N/2); k++)
+            cout << arr[i][k] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
