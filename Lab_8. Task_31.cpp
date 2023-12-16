90c144db17d4a727d837ab77ec7377f519e6b82d
neve7mind <134313494+neve7mind@users.noreply.github.com>
2023-12-16 20:10:58 +0300

    Create Lab_8. Task_31.cpp

---
+++ Lab_8. Task_31.cpp
@@ -0,0 +1,34 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*31. Даны строки S и S0. Проверить, содержится ли строка S0 в строке S. 
+Если содержится, то вывести True, если не содержится, то вывести False.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string S{}, S0{}, res{};
+    getline(cin, S);
+    getline(cin, S0);
+    
+    for(unsigned i=0; i<S.size()-S0.size()+1; i++)
+    {
+        for(unsigned j=0; j<S0.size(); j++)
+        {
+            if(S0[j]!=S[i+j])
+            {
+                res = "False";
+                break;
+            }
+            else 
+            {
+                res = "True";
+            }
+        }
+        if(res=="True")
+            break;
+    }
+    cout << res;
+
+    return 0;
+}
