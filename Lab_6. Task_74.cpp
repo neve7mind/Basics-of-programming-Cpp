7dd340cd2a2a85366ee7e3cadc6a40c98f1ccbf1
neve7mind <134313494+neve7mind@users.noreply.github.com>
2024-02-12 14:01:48 +0300

    Create Lab_6. Task_74.cpp

---
+++ Lab_6. Task_74.cpp
@@ -0,0 +1,46 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*74. Дан массив размера N (N>1). Обнулить элементы массива, расположенные между его минимальным 
+и максимальным элементами (не включая минимальный и максимальный элементы).*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    float arr[N];
+    for(int i=0; i<N; i++)
+        cin >> arr[i];
+    float max=arr[0], min=arr[0];
+    int kmax=0, kmin=0;
+    for(int i=0; i<N; i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+            kmax=i;
+        }
+    }
+    for(int i=0; i<N; i++)
+    {
+        if(arr[i]<min)
+        {
+            min=arr[i];
+            kmin=i;
+        }
+    }
+    if(kmin<kmax)
+    {
+        for(kmin+1; kmin+1<kmax; kmin++)
+            arr[kmin+1]=0;
+    }
+    else{
+        for(kmax+1; kmax+1<kmin; kmax++)
+            arr[kmax+1]=0;
+    }
+    for(int i=0; i<N; i++)
+        cout << arr[i] << " ";
+    
+    return 0;
+}
