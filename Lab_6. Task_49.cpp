75f0d8471a89832ad7b6eae975db65ef7a8f01bd
neve7mind <134313494+neve7mind@users.noreply.github.com>
2024-02-12 14:01:05 +0300

    Create Lab_6. Task_49.cpp

---
+++ Lab_6. Task_49.cpp
@@ -0,0 +1,40 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*49. Дан целочисленный массив размера N (N>1). Если он является перестановкой, то есть содержит все числа от 1 до N, 
+то вывести 0; в противном случае вывести номер первого недопустимого элемента.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int arr[N], k=0;
+    for(int i=0; i<N; i++)
+        cin >> arr[i];
+    
+    for(int i=0; i<N; i++)
+    {
+        if(arr[i]>N || arr[i]<1)
+        {
+            cout << i+1;
+            k++;
+            break;
+        }  
+        for(int j=0; j<N; j++)
+        {
+            if(arr[i]==arr[j] && i!=j)
+            {
+                cout << i+1;
+                k++;
+                break;
+            }
+        }
+        if(k!=0)
+            break;
+    }
+    if(k==0)
+        cout << "0";
+    
+    return 0;
+}
