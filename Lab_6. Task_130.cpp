fef9dcae6422c20421388b1253f8ac3376144077
neve7mind <134313494+neve7mind@users.noreply.github.com>
2024-02-12 14:04:20 +0300

    Create Lab_6. Task_130.cpp

---
+++ Lab_6. Task_130.cpp
@@ -0,0 +1,61 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*130. Дан целочисленный массив размера N. Преобразовать массив, увеличив все его серии наибольшей длины на один элемент.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int max=1;
+    int k=1;
+    int flags[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        flags[i] = 0;
+    }
+    for (int i=1; i<n; i++)
+    {
+        if(arr[i-1]==arr[i])
+        {
+            k++;
+            if(max<k)  
+                max=k;
+        }
+        else k=1;
+    }
+    k=1;
+    for (int i=1; i<n; i++)
+    {
+        if(arr[i-1]==arr[i])
+            k++;
+        else 
+            k=1;
+        if(k==max)
+        {
+            for(int j=i; j>i-max-1; j--)
+                flags[j]=1;
+            if(max>1) 
+                flags[i-max+1] = 2; 
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(flags[i]==0)
+            cout<<arr[i]<<" ";
+        else
+        {
+            if(flags[i]==2 && max>1 || flags[i]==1 && max==1)
+            {
+                for (int j=0; j<max+1; j++)
+                    cout << arr[i] << " ";
+            }
+        }
+    }
+    
+    return 0;
+}
+
