6f3d6126f504b6cff5aa4aa27f60f7ee9e2ae8b2
neve7mind <134313494+neve7mind@users.noreply.github.com>
2024-02-12 13:50:40 +0300

    Create Lab_2. Task_14.cpp

---
+++ Lab_2. Task_14.cpp
@@ -0,0 +1,25 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+// 14. Даны три числа. Найти среднее из них (то есть число, расположенное между наименьшим и наибольшим).
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a, b, c;
+    cout<<"Введите 3 числа: ";
+    cin>>a>>b>>c;
+    cout<<"Среднее число: ";
+    if((a<=c) && (b<=a) || (a>=c) && (b>=a)) 
+        cout<<a;
+    else 
+    {
+        if((a>=b) && (b>=c) || (a<=b) && (b<=c))
+            cout<<b;
+        else
+            cout<<c;
+    }
+
+    return 0;
+}
