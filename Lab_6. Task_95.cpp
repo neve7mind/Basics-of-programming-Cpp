3a5046cbb9032e910e0c05712382f78ce2d8688c
neve7mind <134313494+neve7mind@users.noreply.github.com>
2024-02-12 14:02:16 +0300

    Create Lab_6. Task_95.cpp

---
+++ Lab_6. Task_95.cpp
@@ -0,0 +1,25 @@
+//Мурашов Иван Вячеславович, НКАбд-04-23
+/*95. Дан массив вещественных чисел размера N (N>1). Удалить из массива все соседние одинаковые 
+элементы, оставив их первые вхождения.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    float arr[N];
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
+    }
+    float k = arr[0];
+    cout<<k<<" ";
+    for(int i=1; i<N; i++){
+        if(arr[i]!=k){
+            k = arr[i];
+            cout<<k<<" ";
+        }
+    }
+    return 0;
+}
